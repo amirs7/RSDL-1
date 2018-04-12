b1b0b437017596452204dbd4178bd38089c4dc23
Amir <aseyhani@icloud.com>
2018-04-13 00:03:12 +0430

    refactor example

--- examples/textInput.cpp
+++ examples/textInput.cpp
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-#define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
+#define width 640
+#define heigth 480
 #define MAX_STRING_LENGTH 10
 #define WINDOW_TITLE "RSDL Tutorial"
 #define BACK_SPACE 8
@@ -17,132 +17,110 @@ using namespace std;
 #define CURSOR_AGE 50
 
 #define IMG_SQUARE "examples/assets/square.png"
-#define IMG_BACKGROUND "examples/assets/background.jpg"
-#define FONT_FREESANS "examples/assets/FreeSans.ttf"
-
-//One self defined struct which represents the square on screen.
-struct Square{
-  int x,y;
-  int v_x,v_y;
-  int width,height;
-};
-
-//Initializing the square.
-Square create_square(int x, int y, int v_x, int v_y, int width, int height) {
-  Square square;
-  square.x = x;
-  square.y = y;
-  square.v_x = v_x;
-  square.v_y = v_y;
-  square.width = width;
-  square.height = height;
-  return square;
-}
-
-//This segment reflects the square when it reaches the borders.
-void collide_with_horizontal_border(Square& square)
-{
-  if (square.y<= 0 || square.y > (WINDOW_HEIGHT - square.height) )
-    square.v_y = -square.v_y;
-}
-
-void collide_with_vertical_border(Square& square)
-{
-  if (square.x <= 0 || square.x > (WINDOW_WIDTH - square.width) )
-    square.v_x = -square.v_x;
-}
-
-//Move the square for one frame.
-void move_square(Square& square)
-{
-  square.x += square.v_x;
-  square.y += square.v_y;
-
-  collide_with_horizontal_border(square);
-  collide_with_vertical_border(square);
-}
-
-//One simple function to draw a string on screen.
-void draw_string(window& win, string text)
-{
-  win.show_text(text, 100, 100, WHITE, FONT_FREESANS, 30);
-}
+#define IMG_BACKGROUND "examples/assets/background.jpeg"
+#define FONT_FREESANS "examples/assets/OpenSans.ttf"
 
 //An example of use cases of RSDL functions.
-void draw_dynamic_background(window& win, Square square)
+void drawDynamicBackground(Window* win)
 {
-  win.draw_bg(IMG_BACKGROUND, 0, 0);
-  win.draw_png(IMG_SQUARE, square.x, square.y, square.width, square.height, 45);
-  win.fill_rect(100, 100, (400 - 1), (50 - 1), RED);
+  win->draw_bg(IMG_BACKGROUND, 0, 0);
+  win->fill_rect(100, 100, (400 - 1), (50 - 1), RED);
 }
 
-string prepare_output_text(string input_string)
-{
-  return "  Your name: " + input_string;
-}
 
-void erase_last_char(string& input_string)
-{
-  if(input_string.size() > 0)
-      input_string.erase(input_string.size() - 1);
-}
 
-void add_char(string& input_string, char c)
-{
-  if(input_string.size() < MAX_STRING_LENGTH)
-    input_string += c;
-}
+class MyString{
+public:
+  string prepareOutputText()
+  {
+    return "  Your name: " + inputString;
+  }
 
-void process_rsdl_input(window& win, bool& quit_flag, string& input_string) {
-  //When working with RSDL you first poll for the latest event.
-  //Then you can work with the event.
-  Event event = win.pollForEvent();
+  void eraseLastChar()
+  {
+    if(inputString.size() > 0)
+        inputString.erase(inputString.size() - 1);
+  }
 
-  //List of these events are availabe in rsdl.hpp .
-  if(event.type() == KEY_PRESS) {
-    if(event.pressedKey() == BACK_SPACE)
-      erase_last_char(input_string);
-    else {
-      if(event.pressedKey() == RETURN)
-      	quit_flag = true;
-      else
-        add_char(input_string, event.pressedKey());
+  void addChar(char c)
+  {
+    if(inputString.size() < MAX_STRING_LENGTH)
+      inputString += c;
+  }
+  //A simple function to draw a string on screen.
+  void drawString(Window* win)
+  {
+    win->show_text(prepareOutputText(), 100, 100, WHITE, FONT_FREESANS, 30);
+  }
+
+  string getString(){
+    return inputString;
+  }
+private:
+  string inputString;
+};
+
+class TextInput {
+private:
+  Window* win;
+  MyString inputString;
+public:
+  TextInput(){
+    win = new Window(width, heigth, "Text Input");
+  }
+  void getInput(){
+    bool quitFlag = false;
+
+    //Main loop
+    while(!quitFlag) {
+
+      //Handle input
+      quitFlag = processInput();
+
+      //Draw Section
+      draw();
+
+      //Delay your program for specific amount time.
+      Delay(TICK_DURATION);
     }
   }
-}
-
-void run_input_capture_window(window& win, string& input_string)
-{
-  Square square = create_square(50, 60, 1, -1, 40, 40);
-  bool quit_flag = false;
-
-  while(!quit_flag) {
-    //Input Section
-    process_rsdl_input(win, quit_flag, input_string);
-
-    //Simple Logic
-    move_square(square);
-
-    //Draw Section
-    win.clear();
-    draw_dynamic_background(win, square);
-    draw_string(win, prepare_output_text(input_string));
-    //Update the current window.
-    win.update_screen();
-
-    //Delay your program for specific amount time.
-    Delay(TICK_DURATION);
+  string getInputString(){
+    getInput();
+    return inputString.getString();
   }
-}
+  void draw()
+  {
+    win->clear();
+    drawDynamicBackground(win);
+    inputString.drawString(win);
+    //Update the current Window.
+    win->update_screen();
+  }
+  bool processInput(){
+    Event event = win->pollForEvent();
+    return processRsdlEvent(event);
+  }
+  bool processRsdlEvent(Event event) {
+    bool quitFlag = false;
+    //When working with RSDL you first poll for the latest event.
+    //Then you can work with the event.
+    //List of these events are availabe in rsdl.hpp .
+    if(event.type() == KEY_PRESS) {
+      if(event.pressedKey() == BACK_SPACE)
+        inputString.eraseLastChar();
+      else {
+        if(event.pressedKey() == RETURN)
+        	quitFlag = true;
+        else
+          inputString.addChar(event.pressedKey());
+      }
+    }
+    return quitFlag;
+  }
+};
 
 int main()
 {
-  //  Whenever you're using RSDL, you must first create a new window
-  //to draw your shapes in.
-  window win(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
-
-  string input_string;
-  run_input_capture_window(win, input_string);
-
-	cout<<input_string<<endl;
+  TextInput textInput;
+  cout<<textInput.getInputString()<<endl;
 }
